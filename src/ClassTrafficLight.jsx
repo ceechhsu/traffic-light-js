3f6f92c30f01b400a9e5b6d704aec15490bf4046
Ceech Hsu <ceechhsu@gmail.com>
2023-09-14 20:13:42 -0700

    Complete ClassTrafficLight

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -5,17 +5,33 @@ export class ClassTrafficLight extends Component {
     trafficLightIndex: 0,
   };
 
+  nextState = () => {
+    const { trafficLightIndex } = this.state;
+    if (trafficLightIndex < 2) {
+      this.setState({ trafficLightIndex: trafficLightIndex + 1 });
+    } else {
+      this.setState({ trafficLightIndex: 0 });
+    }
+  };
+
   render() {
+    const { trafficLightIndex } = this.state;
+    const isRed = trafficLightIndex === 0 ? "red" : "black";
+    const isYellow = trafficLightIndex === 1 ? "yellow" : "black";
+    const isGreen = trafficLightIndex === 2 ? "green" : "black";
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${isRed}`}></div>
+          <div className={`circle ${isYellow}`}></div>
+          <div className={`circle ${isGreen}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextState}>
+          Next State
+        </button>
       </div>
     );
   }
